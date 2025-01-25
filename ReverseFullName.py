800635689a6e35b4f980cd122f0574bc536911b8
Partha Majumder <parthamajumder9810@gmail.com>
2025-01-25 23:44:36 +0530

    Reverse a Full name

---
+++ ReverseFullName.py
@@ -0,0 +1,12 @@
+"""
+Expreiment Number : 05
+Expreiment Name : write a py program to a Reverse Full name
+"""
+# Prompt the user to input their first name and store it in the 'fname' variable
+fname = input("Input your First Name : ")
+
+# Prompt the user to input their last name and store it in the 'lname' variable
+lname = input("Input your Last Name : ")
+
+# Display a greeting message with the last name followed by the first name
+print("Hello  " + lname + " " + fname)
